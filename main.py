812f1d9e6f1972225eba8ec80c03d96060ce4c22
zargot <zargot.ripley@gmail.com>
2021-09-28 10:48:49 +0200

    add validate test

---
+++ main.py
@@ -0,0 +1,34 @@
+from typing import TypeVar
+
+T = TypeVar('T')
+
+# legal types are str and int
+def validate(data, schema: dict[str, T]) -> [str]:
+    errors = [str]
+    errors.append("test")
+    return errors
+
+def test():
+    schema = {
+        "email": "string",
+        "phone": "number",
+        "address": "string"
+    }
+    data_one = {}
+    data_two = {
+        "email":"test@email.com",
+        "phone": "12345",
+        "address": "123 Test Way"
+    }
+    data_three = {
+        "email":"test@email.com",
+        "phone": 12345,
+        "address": "123 Test Way",
+        "postalCode": "K6D789"
+    }
+    print(validate(data_one, schema))
+    print(validate(data_two, schema))
+    print(validate(data_three, schema))
+
+if __name__ == "__main__":
+    test()
